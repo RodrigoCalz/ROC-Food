19957daaa5dbde21f8247c93771052b98a4e7321
Rodrigo Calazans Santos <rodrigoocalazans@gmail.com>
2023-03-01 12:44:18 -0300

    rotas

---
+++ roc-food/src/pages/Contato.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contato = () => {
+  return (
+    <div>
+        <h1>Contato</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatibus ad cupiditate placeat 
+            possimus, exercitationem fugiat ipsum eveniet. 
+            Quo minima harum ullam quidem aspernatur fugit deserunt error enim at vitae?
+        </p>
+    </div>
+  )
+}
+
+export default Contato
---
+++ roc-food/src/pages/Empresa.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Empresa = () => {
+  return (
+    <div>
+        <h1>Empresa</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatibus ad cupiditate placeat 
+            possimus, exercitationem fugiat ipsum eveniet. 
+            Quo minima harum ullam quidem aspernatur fugit deserunt error enim at vitae?
+        </p>
+    </div>
+  )
+}
+
+export default Empresa
